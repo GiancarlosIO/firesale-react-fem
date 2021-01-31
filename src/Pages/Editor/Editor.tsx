c8412c7df8cdde2a75d03622cc360da9da4fb53a
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-01-30 19:07:23 -0500

    feat: Implement saveFile and revert handlers

--- src/Pages/Editor/Editor.tsx
+++ src/Pages/Editor/Editor.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-danger */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import path from 'path';
+import { promisify } from 'util';
+import fs from 'fs';
 
 import * as React from 'react';
 import marked from 'marked';
@@ -9,8 +11,17 @@ import { remote } from 'electron';
 
 import type { Utils } from '../../utils';
 
+const writeFile = promisify(fs.writeFile);
 const utils: Utils = remote.require('./utils.ts').default;
 const currentWindow = remote.getCurrentWindow();
+
+type File = {
+  content: string;
+  filename: string;
+  filePath: string;
+  // temporal tells if the current file is not a "real" file. Therefore, the user is just writting in the default textarea
+  temporal?: boolean;
+};
 
 const markedAndPurify = (markdown: string): string => {
   const html = marked(markdown);
@@ -19,14 +30,52 @@ const markedAndPurify = (markdown: string): string => {
 };
 
 const Editor = () => {
+  const [isUsingTmpFile, setIsUsingTmpFile] = React.useState(true);
+  const [currentFileOpened, setCurretFileOpened] = React.useState<
+    File | undefined
+  >(undefined);
   const [rawHTML, setRawHTML] = React.useState('');
   const [htmlRendered, setHTMLRendered] = React.useState('');
+  const [unsavedChanges, setUnsavedChanges] = React.useState(false);
 
+  /** We have 3 handlers that changes the rawHTML, thats why I preffer to use a single useEffect
+   * Instead of calling setHTMLRendered in each of these handlers
+   */
   React.useEffect(() => {
     if (rawHTML) {
       setHTMLRendered(markedAndPurify(rawHTML));
     }
   }, [rawHTML]);
+
+  /**
+   * This useEffect compares the original text from the file and the current editor.
+   * If those are differente then we add the "(unsaved)" in the current window title
+   */
+  React.useEffect(() => {
+    let title = '';
+
+    if (currentFileOpened) {
+      title = `${currentFileOpened.filename} - Firesale`;
+
+      if (currentFileOpened.content !== rawHTML) {
+        setUnsavedChanges(true);
+        title = `${title} (unsaved)`;
+      } else {
+        setUnsavedChanges(false);
+        // title = `${currentFileOpened.filename} - Firesale`;
+      }
+
+      currentWindow.setRepresentedFilename(currentFileOpened.filePath);
+    }
+
+    currentWindow.setTitle(title);
+  }, [currentFileOpened, rawHTML, unsavedChanges]);
+
+  React.useEffect(() => {
+    if (currentFileOpened) {
+      currentWindow.setDocumentEdited(unsavedChanges);
+    }
+  }, [unsavedChanges, currentFileOpened]);
 
   const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const {
@@ -39,10 +88,46 @@ const Editor = () => {
     const result = await utils.getFileFromUser();
 
     if (result) {
-      const { content, pathfile } = result;
+      const { content, filePath } = result;
+      const filename = path.basename(filePath);
+      setCurretFileOpened({
+        content,
+        filename,
+        filePath,
+      });
       setRawHTML(content);
-      const relativePathfile = path.basename(pathfile);
-      currentWindow.setTitle(`${relativePathfile} - Firesale`);
+      setIsUsingTmpFile(false);
+    }
+  };
+
+  const onClickRevert = () => {
+    if (currentFileOpened) {
+      setRawHTML(currentFileOpened.content);
+    }
+  };
+
+  const onClickSave = async () => {
+    if (currentFileOpened) {
+      // 1. save the new content file to the original file
+      await writeFile(currentFileOpened?.filePath, rawHTML);
+
+      // 2. update the currentFileOpened state
+      setCurretFileOpened({
+        ...currentFileOpened,
+        content: rawHTML,
+      });
+    } else {
+      // 3. if there is not file opened, just tell the user where he wants to save the file
+      const newFile = await utils.openSaveDialog();
+      if (!newFile.canceled && newFile.filePath) {
+        await writeFile(newFile.filePath, rawHTML);
+        setCurretFileOpened({
+          content: rawHTML,
+          filePath: newFile.filePath,
+          filename: path.basename(newFile.filePath),
+        });
+        setIsUsingTmpFile(false);
+      }
     }
   };
 
@@ -55,10 +140,20 @@ const Editor = () => {
         <button type="button" id="open-file" onClick={onClickOpenFile}>
           Open File
         </button>
-        <button type="button" id="save-markdown" disabled>
+        <button
+          type="button"
+          id="save-markdown"
+          disabled={!(unsavedChanges || (isUsingTmpFile && rawHTML.length > 0))}
+          onClick={onClickSave}
+        >
           Save File
         </button>
-        <button type="button" id="revert" disabled>
+        <button
+          type="button"
+          id="revert"
+          disabled={!unsavedChanges}
+          onClick={onClickRevert}
+        >
           Revert
         </button>
         <button type="button" id="save-html">
