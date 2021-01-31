2a799b5987a03fa3553769894d3033d8ecd3c86b
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2021-01-30 19:45:24 -0500

    feat: Add the Dialog component based in material-ui/Dialog

---
+++ src/Components/Dialog.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import type { DialogProps } from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const CustomDialog: React.FC<
+  DialogProps & {
+    handleClose: () => void;
+    title: string;
+    textContent: string;
+    handleAgree: () => void;
+    handleDisagree: () => void;
+    disagreeTextButton: string;
+    agreeTextButton: string;
+  }
+> = ({
+  open,
+  handleClose,
+  title,
+  textContent,
+  handleAgree,
+  handleDisagree,
+  disagreeTextButton,
+  agreeTextButton,
+}) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {textContent}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleAgree} color="primary" autoFocus>
+          {agreeTextButton}
+        </Button>
+        <Button onClick={handleDisagree} color="primary">
+          {disagreeTextButton}
+        </Button>
+        <Button onClick={handleClose} color="primary" autoFocus>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default CustomDialog;
